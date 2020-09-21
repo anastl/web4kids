4bcc0f2af70524e42e30470330daef53bcf0b818
anastl <48775643+anastl@users.noreply.github.com>
2020-09-21 10:08:45 -0400

    Delete footer.js

--- Pagina-web/js/footer.js
+++
@@ -1,8 +0,0 @@
-function cpuCLick() {
-    if ( document.getElementById('dropup-content').style.display == 'block' ){
-        document.getElementById('dropup-content').style.display = 'none';
-    }
-    else {
-        document.getElementById('dropup-content').style.display = 'block';
-    }
-}
