28a79d6ed18f6d8874fe075a7696420c8582d9ac
anastl <48775643+anastl@users.noreply.github.com>
2020-09-25 11:49:22 -0400

    Add files via upload

--- js/slide.js
+++ js/slide.js
@@ -1,21 +1,33 @@
 function clickJumbor() {
-
     var current = document.getElementById('shown');
     var pics = document.getElementsByClassName('pic');
     var next = 0;
 
-    for (var i=0; i<pics.length; i++){
-        if (current.src == pics[i].src){
-            next = i;
-            break;
+    if ( document.getElementById('shownbig').style.display != 'none' ){
+        if (document.getElementById('shown').src == pics[0].src){
+            document.getElementById('shown').src = 'img/web-3.jpg';
+            document.getElementById('shownbig').src = 'img/web-4.jpg';
+        }
+        else {
+            document.getElementById('shown').src = 'img/web-1.jpg';
+            document.getElementById('shownbig').src = 'img/web-2.jpg';
         }
     }
+    else {    
 
-    if ( next+1 == pics.length ){
-        document.getElementById('shown').src = pics[0].src;
-    }
-    else {
-        document.getElementById('shown').src = pics[i+1].src;
+        for (var i=0; i<pics.length; i++){
+            if (current.src == pics[i].src){
+                next = i;
+                break;
+            }
+        }
+
+        if ( next+1 == pics.length ){
+            document.getElementById('shown').src = pics[0].src;
+        }
+        else {
+            document.getElementById('shown').src = pics[i+1].src;
+        }
     }
     
 }
@@ -26,18 +38,30 @@ function clickJumbol() {
     var pics = document.getElementsByClassName('pic');
     var next = 0;
 
-    for (var i=0; i<pics.length; i++){
-        if (current.src == pics[i].src){
-            next = i;
-            break;
+    if ( document.getElementById('shownbig').style.display != 'none' ) {
+        if (document.getElementById('shown').src == pics[0].src){
+            document.getElementById('shown').src = 'img/web-3.jpg';
+            document.getElementById('shownbig').src = 'img/web-4.jpg';
+        }
+        else {
+            document.getElementById('shown').src = 'img/web-1.jpg';
+            document.getElementById('shownbig').src = 'img/web-2.jpg';
         }
     }
-
-    if ( next-1 == -1 ){
-        document.getElementById('shown').src = pics[pics.length-1].src;
-    }
     else {
-        document.getElementById('shown').src = pics[next-1].src;
+
+        for (var i=0; i<pics.length; i++){
+            if (current.src == pics[i].src){
+                next = i;
+                break;
+            }
+        }
+
+        if ( next-1 == -1 ){
+            document.getElementById('shown').src = pics[pics.length-1].src;
+        }
+        else {
+            document.getElementById('shown').src = pics[next-1].src;
+        }
     }
-    
 }
